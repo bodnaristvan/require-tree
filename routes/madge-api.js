c53afdf679d01ae290649699418b7601fe47a35d
Istvan Bodnar <istvan@gawker.com>
2015-05-28 16:36:46 +0200

    Return modules that depend on the specified package
    API endpoint is `/api/madge/usedby?module=:modulename`

--- routes/madge-api.js
+++ routes/madge-api.js
@@ -19,4 +19,17 @@ router.get('/', function(req, res, next) {
 	res.send(dependencyObject.tree);
 });
 
+router.get('/usedby', function(req, res, next) {
+	if (!config.madge.path || config.madge.path === '') {
+		res
+			.status(500)
+			.send({
+				error: 'No path set for dependency tree parsing in config.js'
+			});
+		return;
+	}
+	var dependencyObject = madge(config.madge.path, config.madge.options);
+	res.send(dependencyObject.depends(req.query.module));
+});
+
 module.exports = router;
